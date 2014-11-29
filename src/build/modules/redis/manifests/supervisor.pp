c7908dc0552b34e0dfb459432a4010c0cabbe03f
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-29 21:45:16 +0200

    Update

--- src/build/modules/redis/manifests/supervisor.pp
+++ src/build/modules/redis/manifests/supervisor.pp
@@ -1,6 +1,7 @@
 class redis::supervisor {
   file { '/etc/supervisor/conf.d/redis.conf':
     ensure => present,
-    source => 'puppet:///modules/redis/etc/supervisor/conf.d/redis.conf'
+    source => 'puppet:///modules/redis/etc/supervisor/conf.d/redis.conf',
+    mode => 644
   }
 }
