936162f77edee3afbdd170648cf5d8ffbc239f3e
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 20:43:46 +0200

    Update

--- src/build/modules/redis/manifests/packages.pp
+++ src/build/modules/redis/manifests/packages.pp
@@ -1,12 +1,7 @@
 class redis::packages {
-  exec { 'apt-get update':
-    path => ['/usr/bin']
-  }
-
   package {[
       'build-essential'
     ]:
-    ensure => present,
-    require => Exec['apt-get update']
+    ensure => present
   }
 }
